4af3c8fecda5f74309168bef42d06786cce0e234
Nilson Sierota <nilsoncv2@gmail.com>
2022-09-08 16:42:01 -0400

    Test create-appointments

---
+++ src/use-cases/create-appointments.ts
@@ -0,0 +1,17 @@
+import { Appointment } from '../entities/appointments'
+
+interface CreateAppointmentRequest {
+    customer: string
+    startsAt: Date
+    endsAt: Date
+}
+
+type CreateAppointmentResponse = Appointment
+
+export class CreateAppointment {
+  async execute ({ customer, startsAt, endsAt } : CreateAppointmentRequest) : Promise<CreateAppointmentResponse> {
+    const appointment = new Appointment({ customer, startsAt, endsAt })
+
+    return appointment
+  }
+}
