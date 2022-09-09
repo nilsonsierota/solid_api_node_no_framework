2de64222ccb3f2b4e73379b922b3e786088f4913
Nilson Sierota <nilsoncv2@gmail.com>
2022-09-09 09:13:33 -0400

    utils date and change date test

--- src/entities/appointments.spec.ts
+++ src/entities/appointments.spec.ts
@@ -1,11 +1,10 @@
 import { expect, test } from 'vitest'
+import { getFutureDate } from '../tests/utils/get-future-date'
 import { Appointment } from './appointments'
 
 test('create an appointment', () => {
-  const startsAt = new Date()
-  const endsAt = new Date()
-
-  endsAt.setDate(endsAt.getDate() + 1)
+  const startsAt = getFutureDate('2022-08-10')
+  const endsAt = getFutureDate('2022-08-11')
 
   const appointment = new Appointment({
     customer: 'John Doe',
@@ -17,11 +16,25 @@ test('create an appointment', () => {
   expect(appointment.customer).toEqual('John Doe')
 })
 
-test('cannot creater an appointment with end date before start date', () => {
+test('cannot create an appointment with end date before start date', () => {
+  const startsAt = getFutureDate('2022-08-10')
+  const endsAt = getFutureDate('2022-08-09')
+
+  expect(() => {
+    return new Appointment({
+      customer: 'John Doe',
+      startsAt,
+      endsAt
+    })
+  }).toThrow()
+})
+
+test('cannot create an appointment with start date before now', () => {
   const startsAt = new Date()
   const endsAt = new Date()
 
-  endsAt.setDate(endsAt.getDate() - 1)
+  startsAt.setDate(startsAt.getDate() - 1)
+  endsAt.setDate(endsAt.getDate() + 3)
 
   expect(() => {
     return new Appointment({
--- src/use-cases/create-appointments.spec.ts
+++ src/use-cases/create-appointments.spec.ts
@@ -1,15 +1,14 @@
 import { describe, expect, it } from 'vitest'
 import { Appointment } from '../entities/appointments'
+import { getFutureDate } from '../tests/utils/get-future-date'
 import { CreateAppointment } from './create-appointments'
 
 describe('Create Appointment', () => {
   it('should be able to create an appointment', () => {
+    const startsAt = getFutureDate('2022-08-10')
+    const endsAt = getFutureDate('2022-08-11')
+
     const createAppointment = new CreateAppointment()
-
-    const startsAt = new Date()
-    const endsAt = new Date()
-
-    endsAt.setDate(endsAt.getDate() + 1)
 
     expect(createAppointment.execute({ customer: 'John Doe', startsAt, endsAt }))
       .resolves.toBeInstanceOf(Appointment)
